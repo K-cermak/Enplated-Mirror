5de88d3534a1776312ef3e8fccefaaa70a583eb2
EnplatedSyncer <karel4cermak@seznam.cz>
2023-07-18 19:57:49 +0200

    Deleted old files

--- settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define("ACCESS_CODE", "12345");
-    define("ROOT_FOLDER", "/var/www/html/mydata/");
-    define("SERVER_ROOT", "/var/www/html/");
-    define("DISK_NAME", "My Drive");
-    define("APP_NAME", "Enplated Mirror");
-    define("FAVICON_LOCATION", "partials/images/favicon.png");
-    define("FOOTER_MESSAGE", "License granted to Karlosoft Group.");
-?>
